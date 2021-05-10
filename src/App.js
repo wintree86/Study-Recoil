f8d392a1ec5430a83c7cff7e5d87a75c33ad9241
wintree86 <mith1004@gmail.com>
2021-05-10 18:59:14 +0900

    [update] 예제 컴포넌트 폴더로 이동

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
 import React from "react";
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
-
-const textState = atom({
-  key: "textState",
-  default: "",
-});
-
-const charCountState = selector({
-  key: "charCountState",
-  get: ({ get }) => {
-    const text = get(textState);
-    return text.length;
-  },
-});
+import Example from './components/Example'
 
 function App() {
   return (
-    <RecoilRoot>
-      <CharacterCounter />
-    </RecoilRoot>
+    <React.Fragment>
+      <Example/>
+    </React.Fragment>
   );
-}
-
-function CharacterCounter() {
-  return (
-    <div>
-      <TextInput />
-      <CharacterCount />
-    </div>
-  );
-}
-
-function TextInput() {
-  const [text, setText] = useRecoilState(textState);
-  const onChange = (event) => {
-    setText(event.target.value);
-  };
-
-  return (
-    <div>
-      <input type="text" value={text} onChange={onChange} />
-      <br />
-      Echo: {text}
-    </div>
-  );
-}
-
-function CharacterCount() {
-  const count = useRecoilValue(charCountState);
-  return <>Character Count: {count}</>;
 }
 
 export default App;
---
+++ src/components/Example/Example.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+
+const textState = atom({
+  key: "textState",
+  default: "",
+});
+
+const charCountState = selector({
+  key: "charCountState",
+  get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  },
+});
+
+function Example() {
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+  return <>Character Count: {count}</>;
+}
+
+export default Example;
